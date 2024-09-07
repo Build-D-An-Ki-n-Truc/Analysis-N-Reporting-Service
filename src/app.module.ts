09490e578f225ac89998f7c2476df3ca7e269d48
HaiMen <mikegaming400@gmail.com>
2024-09-07 13:19:55 +0700

    Updated Dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GrafanaModule } from './grafana/grafana.module';
 import { HealthModule } from './health/health.module';
+import { NatsClientModule } from './nats-client/nats-client.module';
 
 @Module({
   imports: [GrafanaModule, HealthModule],
